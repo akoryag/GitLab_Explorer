03dfca5771ebffb9b331a4311243026b313f6dd5
Алексей Корягин <koryaginai@it2g.ru>
2025-09-24 22:45:19 +0300

    Создавние вкладок

--- static/app.js
+++ static/app.js
@@ -275,3 +275,10 @@ async function openPipeline(groupIdx, projIdx, projectId) {
     alert("Не удалось получить URL пайплайна");
   }
 }
+
+function openTab(tabId) {
+  document.querySelectorAll('.tab-content').forEach(el => el.classList.remove('active'));
+  document.querySelectorAll('.tab').forEach(el => el.classList.remove('active'));
+  document.getElementById(tabId).classList.add('active');
+  event.target.classList.add('active');
+}
